656c8a6faaaca03372723ab84e060695bd7667c4
kkupcin <kkupcin@gmail.com>
2022-01-10 23:14:24 +0000

    add Work In Progress message

---
+++ src/components/Wip.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import styles from "../styles/Wip.module.css";
+
+const Wip = () => {
+  const [closeWip, setCloseWip] = useState(false);
+
+  const onCloseWip = () => {
+    setCloseWip(true);
+  };
+
+  return (
+    <React.Fragment>
+      {!closeWip && (
+        <div className={styles.wipBox} onClick={onCloseWip}>
+          <div className={styles.wip}>
+            <h1 className={styles.close}>X</h1>
+            <h3>This website is a work in progress!</h3>
+            <p>Editing of data is disabled.</p>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Wip;

d92f4a11617884637e539d04018d2e87be346afd
kkupcin <kkupcin@gmail.com>
2022-01-11 14:39:56 +0000

    add upvote functionality

--- src/components/FeedbackDetails.js
+++ src/components/FeedbackDetails.js
@@ -2,17 +2,53 @@ import styles from "../styles/FeedbackDetails.module.css";
 import arrowUp from "../assets/shared/icon-arrow-up.svg";
 import commentIcon from "../assets/shared/icon-comments.svg";
 import arrowUpWhite from "../assets/shared/icon-arrow-up-white.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Parse from "parse";
 
 const FeedbackDetails = (props) => {
-  const [upvoteClicked, setUpvoteClicked] = useState(false);
+  const [upvoteClicked, setUpvoteClicked] = useState();
 
   const upvoteClickHandler = () => {
-    setUpvoteClicked(!upvoteClicked);
+    onUpvoteClickSubmit();
   };
 
-  const feedbackClickHandler = () => {
-    props.onFeedbackClick(props.info.id);
+  useEffect(() => {
+    const checkUpvotes = () => {
+      let userUpvoteArray = props.info.get("userUpvotes");
+      if (!userUpvoteArray.includes(Parse.User.current().id)) {
+        setUpvoteClicked(false);
+      } else if (userUpvoteArray.includes(Parse.User.current().id)) {
+        setUpvoteClicked(true);
+      }
+    };
+    checkUpvotes();
+  }, []);
+
+  async function onUpvoteClickSubmit() {
+    let userUpvoteArray = props.info.get("userUpvotes");
+    if (!userUpvoteArray.includes(Parse.User.current().id)) {
+      userUpvoteArray.push(Parse.User.current().id);
+      setUpvoteClicked(true);
+    } else if (userUpvoteArray.includes(Parse.User.current().id)) {
+      const index = userUpvoteArray.indexOf(Parse.User.current().id);
+      userUpvoteArray.splice(index, 1);
+      setUpvoteClicked(false);
+    }
+
+    props.info.set("userUpvotes", userUpvoteArray);
+    props.info.save();
+  }
+
+  const feedbackClickHandler = (e) => {
+    console.log(e.target.parentElement.id, e.target.id);
+    if (
+      e.target.parentElement.id !== "upvoteContainer" &&
+      e.target.id !== "upvoteContainer"
+    ) {
+      if (props.onFeedbackClick) {
+        props.onFeedbackClick(props.info.id);
+      }
+    }
   };
 
   return (
@@ -44,11 +80,24 @@ const FeedbackDetails = (props) => {
         id="upvoteContainer"
       >
         {upvoteClicked ? (
-          <img src={arrowUpWhite} alt="arrow" className={styles.arrow}></img>
+          <img
+            src={arrowUpWhite}
+            alt="arrow"
+            className={styles.arrow}
+            id="upvoteIconWhite"
+          ></img>
         ) : (
-          <img src={arrowUp} alt="arrow" className={styles.arrow}></img>
+          <img
+            src={arrowUp}
+            alt="arrow"
+            className={styles.arrow}
+            id="upvoteIconUp"
+          ></img>
         )}
-        <h4 className={`${styles.counter} ${styles.upvotes}`}>
+        <h4
+          className={`${styles.counter} ${styles.upvotes}`}
+          id="upvoteCounter"
+        >
           {props.info.get("userUpvotes").length}
         </h4>
       </div>
